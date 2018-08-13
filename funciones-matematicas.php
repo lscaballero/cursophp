45595de5c7afae017958bf7fe733a8ae5ae74888
Alfredo <alfreddocaballero@gmail.com>
2018-08-13 10:20:25 -0500

    video 14 de curso php

---
+++ funciones-matematicas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php
+	
+	$num1=5.25343453;
+
+	echo "El numero es: " . round($num1,2);
+
+
+
+
+ ?>
+	
+</body>
+</html>
